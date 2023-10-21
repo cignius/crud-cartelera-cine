12aef838d19042fc5d60a175b5afb66b556f8dd2
Irwing Soto <irwings@holagod.com>
2023-10-21 05:21:59 -0600

    Se añade plantilla de login y comportamiento de validación

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -36,5 +37,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+            'captcha' => 'required|captcha',
+        ]);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    {{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -70,5 +70,199 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+    <style>
+        @import url(https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap);
+
+        body {
+            background: #ffffff;
+        }
+
+        @media only screen and (max-width: 767px) {
+            .hide-on-mobile {
+                display: none;
+            }
+        }
+
+        .login-box {
+            background: url(https://i.imgur.com/73BxBuI.png);
+            background-size: cover;
+            background-position: center;
+            padding: 50px;
+            margin: 50px auto;
+            min-height: 600px;
+            max-width: 70%;
+            -webkit-box-shadow: 0 2px 60px -5px rgba(0, 0, 0, 0.1);
+            box-shadow: 0 2px 60px -5px rgba(0, 0, 0, 0.1);
+        }
+
+        .logo {
+            font-family: "Script MT";
+            font-size: 54px;
+            text-align: center;
+            color: #888888;
+            margin-bottom: 50px;
+        }
+
+        .logo .logo-font {
+            color: #3BC3FF;
+        }
+
+        @media only screen and (max-width: 767px) {
+            .logo {
+                font-size: 34px;
+            }
+        }
+
+        .header-title {
+            text-align: center;
+            margin-bottom: 50px;
+        }
+
+        .login-form {
+            max-width: 300px;
+            margin: 0 auto;
+        }
+
+        .login-form .form-control {
+            border-radius: 0;
+            margin-bottom: 30px;
+        }
+
+        .login-form .form-group {
+            position: relative;
+        }
+
+        .login-form .form-group .forgot-password {
+            position: absolute;
+            top: 6px;
+            right: 15px;
+        }
+
+        .login-form .btn {
+            border-radius: 0;
+            -webkit-box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            margin-bottom: 30px;
+        }
+
+        .login-form .btn.btn-primary {
+            background: #3BC3FF;
+            border-color: #31c0ff;
+        }
+
+        .slider-feature-card {
+            background: #fff;
+            max-width: 280px;
+            margin: 0 auto;
+            padding: 30px;
+            text-align: center;
+            -webkit-box-shadow: 0 2px 25px -3px rgba(0, 0, 0, 0.1);
+            box-shadow: 0 2px 25px -3px rgba(0, 0, 0, 0.1);
+        }
+
+        .slider-feature-card img {
+            height: 80px;
+            margin-top: 30px;
+            margin-bottom: 30px;
+        }
+
+        .slider-feature-card h3,
+        .slider-feature-card p {
+            margin-bottom: 30px;
+        }
+
+        .carousel-indicators {
+            bottom: -50px;
+        }
+
+        .carousel-indicators li {
+            cursor: pointer;
+        }
+    </style>
+    <section class="body">
+        <div class="container-fluid g-0">
+            <div class="login-box">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="logo">
+                            <span class="logo-font">Log</span>in
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <br>
+                        <form method="POST" action="{{ route('login') }}" class="login-form">
+                            @csrf
+                            <div class="form-group">
+                                <input id="email" type="email" placeholder="Correo electrónico" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <input id="password" placeholder="Contraseña" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            </div>
+                            <div class="form-group">
+                                {!! Captcha::img() !!}
+                                <input type="text" id="captcha" class="form-control mt-2" name="captcha"
+                                    placeholder="Escriba la palabra del captcha">
+                            </div>
+                            <div class="form-group text-end">
+                                <button class="btn btn-primary btn-block" type="submit">Iniciar sesión</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-sm-6 hide-on-mobile">
+                        <div id="demo" class="carousel slide" data-bs-ride="carousel">
+                            <!-- Indicators -->
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"
+                                    aria-current="true"></button>
+                                <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+                            </div>
+                            <!-- The slideshow -->
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <div class="slider-feature-card">
+                                        <img src="https://i.imgur.com/YMn8Xo1.png" alt="">
+                                        <h3 class="slider-title">Title Here</h3>
+                                        <p class="slider-description">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                            elit. Iure, odio!</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="slider-feature-card">
+                                        <img src="https://i.imgur.com/Yi5KXKM.png" alt="">
+                                        <h3 class="slider-title">Title Here</h3>
+                                        <p class="slider-description">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                            elit. Ratione, debitis?</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Left and right controls -->
+                            <button class="carousel-control-prev" type="button" data-bs-target="#demo"
+                                data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#demo"
+                                data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
