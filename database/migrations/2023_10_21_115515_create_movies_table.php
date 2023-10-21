a795a868e6a98a180bceb5f504adbd5374f61b2e
Irwing Soto <irwings@holagod.com>
2023-10-21 07:27:32 -0600

    Se añade el Controller, Model, y Migration de peliculas

---
+++ database/migrations/2023_10_21_115515_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('director');
+            $table->string('duration', 5);
+            $table->string('classification',10);
+            $table->timestamp('start_exhibition')->nullable();
+            $table->timestamp('finish_exhibition')->nullable();
+            $table->string('status', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
